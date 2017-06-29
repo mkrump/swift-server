0dc8aab42ecc473e684d8fc644d84af5b4cf4de2
mkrump <mkrump@8thlight.com>
2017-06-29 16:57:18 -0500

    Add acceptance test setup scripts

---
+++ acceptance_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR/cob_spec
+if [ ! -d $DIR/cob_spec ]; then
+    git clone git@github.com:8thlight/cob_spec.git cob_spec
+fi
+cd $DIR/cob_spec
+mvn package
+cp $DIR/cob_spec_config.txt $DIR/cob_spec/FitNesseRoot/HttpTestSuite/content.txt
+java -jar fitnesse.jar -c "HttpTestSuite?suite&format=text"
