7ec6de8f26d3180d6b747c09ad95ab7c51dd80f0
mkrump <mkrump@8thlight.com>
2017-08-03 09:48:30 -0500

    WIP

--- SwiftServer/Tests/HTTPRequestTests/BasicAuthTests.swift
+++ SwiftServer/Tests/HTTPRequestTests/BasicAuthTests.swift
@@ -6,18 +6,29 @@ func decodeBasicAuth(challengeResponse: String) -> (userName: String, passWord: 
     let parsedChallengeResponse = challengeResponse.components(separatedBy: " ")
     if let data = Data(base64Encoded: parsedChallengeResponse[1]),
        let decodedChallenge = String(data: data, encoding: .utf8) {
-        let params = decodedChallenge.components(separatedBy: ":")
-        return (userName: params[0], passWord: params[1])
+        let credentials = decodedChallenge.components(separatedBy: ":")
+        return (userName: credentials[0], passWord: credentials[1])
     }
     return nil
 }
 
+func authSuccess(headers: HeaderParse?, userName: String, password: String) -> Bool {
+    guard let headers = headers,
+          let headerDict = headers.headerDict,
+          let base64EncodedCredentials = headerDict["Authorization"],
+          let credentials = decodeBasicAuth(challengeResponse: base64EncodedCredentials) else {
+        return false
+    }
+    return credentials.userName == userName && credentials.passWord == password
+}
+
 class BasicAuthTests: XCTestCase {
-    func testBasicAuthValid() {
+    func testBasicAuthDecode() {
         let userName = "admin"
         let passWord = "hunter2"
-        let mockHeaders = MockHeaders(rawHeaders: "Authorization: Basic YWRtaW46aHVudGVyMg==\r\n",
-                headerDict: ["Authorization": "Basic YWRtaW46aHVudGVyMg=="])
+        let base64EncodedString = Data((userName + ":" + passWord).utf8).base64EncodedString()
+        let mockHeaders = MockHeaders(rawHeaders: "Authorization: Basic " + base64EncodedString,
+                headerDict: ["Authorization": "Basic " + base64EncodedString])
         guard let headerDict = mockHeaders.headerDict,
               let credentials = headerDict["Authorization"],
               let params = decodeBasicAuth(challengeResponse: credentials) else {
@@ -26,5 +37,24 @@ class BasicAuthTests: XCTestCase {
         }
         XCTAssertEqual(params.userName, userName)
         XCTAssertEqual(params.passWord, passWord)
+    }
+
+    func testBasicAuthCorrect() {
+        let userName = "admin"
+        let passWord = "hunter2"
+        let base64EncodedString = Data((userName + ":" + passWord).utf8).base64EncodedString()
+        let mockHeaders = MockHeaders(rawHeaders: "Authorization: Basic " + base64EncodedString,
+                headerDict: ["Authorization": "Basic " + base64EncodedString])
+        XCTAssertTrue(authSuccess(headers: mockHeaders, userName: userName, password: passWord))
+    }
+
+    func testBasicAuthIncorrect() {
+        let userName = "admin"
+        let passWord = "hunter2"
+        let passwordAttempt = "wrong"
+        let base64EncodedString = Data((userName + ":" + passwordAttempt).utf8).base64EncodedString()
+        let mockHeaders = MockHeaders(rawHeaders: "Authorization: Basic " + base64EncodedString,
+                headerDict: ["Authorization": "Basic " + base64EncodedString])
+        XCTAssertFalse(authSuccess(headers: mockHeaders, userName: userName, password: passWord))
     }
 }
